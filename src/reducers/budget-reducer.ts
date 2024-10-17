0de944f4c9334853632f1bf593d1d70afa57193c
Alain Sosa <alain.sosa.v@gmail.com>
2024-10-17 16:40:08 -0400

    Creando el AMounDIsplay y formatcurrency

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,10 +1,8 @@
 export type BudgetActions = { type: "add-budget"; payload: { budget: number } };
 
-export type BudgetState =
-  | {
-      budget: number;
-    }
-  | undefined;
+export type BudgetState = {
+  budget: number;
+};
 
 export const initialState: BudgetState = {
   budget: 0,
